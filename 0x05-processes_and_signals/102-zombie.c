a24f92c7a9bb867ac0b375498be9569e1c208e3d
ElkinAMG <elkinandresmejiagueto@gmail.com>
2020-06-23 00:00:49 -0500

    Fixed Bug

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,8 +1,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int printf(const char *format, ...);
 int infinite_while(void);
 
 /**
@@ -16,13 +16,11 @@ int main(void)
 	int i;
 	pid_t child_pid;
 
-	printf("Parent → %i\n", getpid());
-
 	for (i = 0; i < 5; i++)
 	{
 		child_pid = fork();
 		if (child_pid > 0)
-			sleep(50);
+			sleep(5);
 		else
 		{
 			printf("Zombie process created, PID: %i\n", getpid());
